0aa5b4a91676c89ed9ac507aa2c261705c8af6b5
TOJI <153635981+Tglnt@users.noreply.github.com>
2023-12-12 20:37:45 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function showTab(tabId) {
+    // Hide all tab contents
+    const tabContents = document.querySelectorAll('.tab-content');
+    tabContents.forEach(content => {
+        content.style.display = 'none';
+    });
+
+    // Show the selected tab content
+    const selectedTab = document.getElementById(tabId);
+    if (selectedTab) {
+        selectedTab.style.display = 'block';
+    }
+}
